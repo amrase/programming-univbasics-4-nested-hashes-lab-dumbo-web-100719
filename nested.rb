dbc4894a88555b3990dad0f15d199d27b5850d4e
Amra Sezairi <amra.sezairi@hotmail.com>
2019-10-03 20:58:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,6 +133,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages][1]
+     dennis_new_value="Assembly"
+     programmer_hash[:dennis_ritchie][:languages][1] 
 
 end
